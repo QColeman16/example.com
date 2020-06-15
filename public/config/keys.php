2a6ec44fdd66be28e84d2d7e370d829b15f6d920
Qudamah Coleman <cfcinc.88#gmail.com>
2020-06-15 14:30:04 -0500

    go

--- public/config/keys.php
+++ public/config/keys.php
@@ -1,3 +1,4 @@
 <?php
 define('MG_KEY', 'd1fe11fb8b04988dea7c08d807795abaf-8b34de1b-973667ba');
+define('MG_API', 'https://api.mailgun.net/v3/sandboxb5d218b398ab4f95b0ed24cc7d90bd9d.mailgun.org/messages');
 define('MG_DOMAIN', 'sandboxb5d218b398ab4f95b0ed24cc7d90bd9d.mailgun.org');
--- public/posts/login.php
+++ public/posts/login.php
@@ -2,6 +2,7 @@
 
 // 1. Require your session file, this contains session_start()
 require '../core/sessions.php';
+require '../core/db_connect.php';
 
 // 2. Listen for a POST request. On Post set a session. 
 // In the next iteration we replace this with authentication logic.
@@ -15,10 +16,59 @@ if(!empty($_POST)){
 // 3. Provide just enough of a form to initiate a POST
 $content=<<<EOT
 
-<form method="post">
-    <input name="goto" value="{$_GET['goto']}">
+form method="post">
+
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input 
+            class="form-control" 
+            id="email" 
+            name="email" 
+            type="email"
+        >
+    </div>
+
+    <div class="form-group">
+        <label for="first_name">First Name</label>
+        <input 
+            class="form-control" 
+            id="first_name" 
+            name="first_name" 
+        >
+    </div>
+
+
+    <div class="form-group">
+        <label for="last_name">Last Name</label>
+        <input 
+            class="form-control" 
+            id="last_name" 
+            name="last_name" 
+        >
+    </div>
+
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input 
+            class="form-control" 
+            id="password" 
+            name="password" 
+            type="password"
+        >
+    </div>
+
+    <div class="form-group">
+        <label for="confirm_password">Password</label>
+        <input 
+            class="form-control" 
+            id="password" 
+            name="confirm_password" 
+            type="confirm_password"
+        >
+    </div>
+
     <input type="submit" class="btn btn-primary">
+
 </form>
 EOT;
-
 require '../core/layout.php';
