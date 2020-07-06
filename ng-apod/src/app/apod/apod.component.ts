6095b8491f93e464efff87f1eb0c80e4ed64b3da
Qudamah Coleman <cfcinc.88#gmail.com>
2020-07-06 14:08:21 -0500

    date changes

--- ng-apod/src/app/apod/apod.component.ts
+++ ng-apod/src/app/apod/apod.component.ts
@@ -12,6 +12,8 @@ import { Apod } from '../models/apod';
 export class ApodComponent implements OnInit {
 
   apod:Apod;
+  date:string;
+
   constructor(
     private apodService: ApodService,
     //2. Inject ActivatedRoute into the constructor
@@ -31,12 +33,23 @@ export class ApodComponent implements OnInit {
     this.apodService.getApod(date).subscribe(
       (response:any)=>{
         this.apod = response;
-
-        //5. Log the results to the JS console
-        console.log(response);
+        //3.  Update this.date on each API call
+        this.date = this.randomDate(new Date(1995,5,16), new Date());
+        //4. Log this.date to the JS console
+        console.log(this.apod);
       }
     );
 
+  }
+
+  //2. Create a method that returns a random date
+  randomDate(start, end): string{
+    let date = new Date(
+      start.getTime() + Math.random() *
+        (end.getTime() - start.getTime())
+    );
+
+    return new Date(date).toISOString().slice(0,10);
   }
 
 }
